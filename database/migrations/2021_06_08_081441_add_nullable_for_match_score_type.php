308446f260d40e9dbd7a58087a72af0dd1052141
vu thanh cong <congv920@gmail.com>
2021-06-24 15:49:31 +0700

    fix database

--- database/migrations/2021_06_08_081441_add_nullable_for_match_score_type.php
+++ database/migrations/2021_06_08_081441_add_nullable_for_match_score_type.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AddNullableForMatchScoreType extends Migration
 {
@@ -14,7 +15,7 @@ class AddNullableForMatchScoreType extends Migration
     public function up()
     {
         //
-        DB::statement("ALTER TABLE `graduation-info`.`stages`
+        DB::statement("ALTER TABLE " . env('DB_DATABASE') . ".`stages`
        CHANGE COLUMN `match_score_type` `match_score_type` TINYINT(3) UNSIGNED NULL
        ");
     }
